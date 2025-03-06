6a3027167bfe73833d96f9f147f7d17c76189950
vinicius-a-almeida <vinicius45anacleto@gmail.com>
2025-03-06 14:12:26 -0300

    gunnicorn setup

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,7 +2,7 @@
 
 source env/bin/activate
 
-cd /var/lib/jenkins/workspace/django-cicd/s3_cognito_example
+cd /var/lib/jenkins/workspace/django-cicd
 
 python3 manage.py makemigrations
 python3 manage.py migrate
