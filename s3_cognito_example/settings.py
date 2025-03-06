0c804bba34bec9f4127c5fed9b58208313a0956a
vinicius-a-almeida <vinicius45anacleto@gmail.com>
2025-03-06 14:30:40 -0300

    gunnicorn setup

--- s3_cognito_example/settings.py
+++ s3_cognito_example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-99wi-h-av9j6**y#dgddpj-dtufyp8(gb%n%0f1mbf$ou-abv&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
